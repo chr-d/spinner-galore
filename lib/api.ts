b741b3fa703bb674862d1b3eed1da6e67ffc05b6
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-09 16:36:13 +0200

    add fetch functions

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,52 @@
 "use server";
 
-// TODO 1: Erstelle getPosts(), getPost(id), getPokemonList() und getPokemon(id).
+import {
+  PokemonListSchema,
+  PokemonSchema,
+  PostSchema,
+  PostsSchema,
+} from "@/lib/schemas";
+
+// 1: Erstelle getPosts(), getPost(id), getPokemonList() und getPokemon(id).
 // Jede Funktion: fetch → response.ok prüfen → response.json() → Schema.parse(data).
 // Die Schemas findest du in schemas.ts.
+
+const getPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) {
+    throw new Error("Failed to load posts");
+  }
+  const data: unknown = await response.json();
+  return PostsSchema.parse(data);
+};
+
+const getPost = async (id: string) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to load post");
+  }
+  const data: unknown = await response.json();
+  return PostSchema.parse(data);
+};
+
+const getPokemonList = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+  if (!response.ok) {
+    throw new Error("Failed to load Pokemons");
+  }
+  const data: unknown = await response.json();
+  return PokemonListSchema.parse(data);
+};
+
+const getPokemon = async (id: string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  if (!response.ok) {
+    throw new Error("Failed to load Pokemon");
+  }
+  const data: unknown = await response.json();
+  return PokemonSchema.parse(data);
+};
+
+export { getPosts, getPost, getPokemonList, getPokemon };
